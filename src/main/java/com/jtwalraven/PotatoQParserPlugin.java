816662cfb642884962f28756a29934c440f339a9
Jeffrey Walraven <jeffrey@ravenswall.net>
2018-04-24 21:58:41 -0400

    Created QParserPlugin

--- src/main/java/com/jtwalraven/PotatoQParserPlugin.java
+++ src/main/java/com/jtwalraven/PotatoQParserPlugin.java
@@ -1,4 +1,13 @@
 package com.jtwalraven;
 
-public class PotatoQParserPlugin {
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.search.QParser;
+import org.apache.solr.search.QParserPlugin;
+
+public class PotatoQParserPlugin extends QParserPlugin {
+    @Override
+    public QParser createParser(String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req) {
+        return new PotatoQParser(qstr, localParams, params, req);
+    }
 }
